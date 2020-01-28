fa66e2afa6f43034a564ded0f4ee8a6555f7160d
David Tolnay <dtolnay@gmail.com>
2020-01-27 23:05:22 -0800

    Customize usage message of cmd

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -7,7 +7,17 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "cxxbridge", author)]
+#[structopt(
+    name = "cxxbridge",
+    author,
+    about = "https://github.com/dtolnay/cxx",
+    usage = "\
+    cxxbridge <input>.rs              Emit .cc file for bridge to stdout
+    cxxbridge <input>.rs --header     Emit .h file for bridge to stdout
+    cxxbridge --header                Emit cxxbridge.h header to stdout",
+    help_message = "Print help information",
+    version_message = "Print version information"
+)]
 struct Opt {
     /// Input Rust source file containing #[cxx::bridge]
     #[structopt(parse(from_os_str), required_unless = "header")]
