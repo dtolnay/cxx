792d02222acf3fd0374aa59df0d285b7e4ec56cd
David Tolnay <dtolnay@gmail.com>
2020-03-16 21:57:47 -0700

    Add niche to RustStr
    This will be useful for enabling fallible functions to return
    Option<RustStr> as the error message if one occurred.

--- src/rust_str.rs
+++ src/rust_str.rs
@@ -1,3 +1,5 @@
+use std::mem;
+use std::ptr::NonNull;
 use std::slice;
 use std::str;
 
@@ -5,20 +7,20 @@ use std::str;
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct RustStr {
-    ptr: *const u8,
+    ptr: NonNull<u8>,
     len: usize,
 }
 
 impl RustStr {
     pub fn from(s: &str) -> Self {
         RustStr {
-            ptr: s.as_ptr(),
+            ptr: NonNull::from(s).cast::<u8>(),
             len: s.len(),
         }
     }
 
     pub unsafe fn as_str<'a>(self) -> &'a str {
-        let slice = slice::from_raw_parts(self.ptr, self.len);
+        let slice = slice::from_raw_parts(self.ptr.as_ptr(), self.len);
         str::from_utf8_unchecked(slice)
     }
 }
@@ -28,3 +30,5 @@ unsafe extern "C" fn str_valid(ptr: *const u8, len: usize) -> bool {
     let slice = slice::from_raw_parts(ptr, len);
     str::from_utf8(slice).is_ok()
 }
+
+const_assert!(mem::size_of::<Option<RustStr>>() == mem::size_of::<RustStr>());
