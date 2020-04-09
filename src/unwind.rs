4ad8020fc4eec61e4cfb2b6efb75333ba1dbdf17
David Tolnay <dtolnay@gmail.com>
2020-04-08 21:16:44 -0700

    Accept non-UnwindSafe extern Rust types in extern Rust signatures

--- src/unwind.rs
+++ src/unwind.rs
@@ -1,12 +1,16 @@
 use std::io::{self, Write};
-use std::panic::{self, UnwindSafe};
+use std::panic::{self, AssertUnwindSafe};
 use std::process;
 
 pub fn catch_unwind<F, R>(label: &'static str, foreign_call: F) -> R
 where
-    F: FnOnce() -> R + UnwindSafe,
+    F: FnOnce() -> R,
 {
-    match panic::catch_unwind(foreign_call) {
+    // Regarding the AssertUnwindSafe: we immediately abort on panic so it
+    // doesn't matter whether the types involved are unwind-safe. The UnwindSafe
+    // bound on catch_unwind is about ensuring nothing is in a broken state if
+    // your program plans to continue after the panic.
+    match panic::catch_unwind(AssertUnwindSafe(foreign_call)) {
         Ok(ret) => ret,
         Err(_) => abort(label),
     }
