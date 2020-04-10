4b972729954b19d0b8535b550997e5caec7fc901
David Tolnay <dtolnay@gmail.com>
2020-04-10 00:48:18 -0700

    Release 0.2.6

--- src/lib.rs
+++ src/lib.rs
@@ -337,7 +337,7 @@
 //!
 //! [https://github.com/dtolnay/cxx]: https://github.com/dtolnay/cxx
 
-#![doc(html_root_url = "https://docs.rs/cxx/0.2.5")]
+#![doc(html_root_url = "https://docs.rs/cxx/0.2.6")]
 #![deny(improper_ctypes)]
 #![allow(
     clippy::cognitive_complexity,
