9d8d80bd811811067c6bae4f13cbdea152550887
David Tolnay <dtolnay@gmail.com>
2020-04-19 21:40:04 -0700

    Remove '_' segment from symbol of non-associated functions

--- syntax/mangle.rs
+++ syntax/mangle.rs
@@ -2,9 +2,9 @@ use crate::syntax::namespace::Namespace;
 use crate::syntax::ExternFn;
 
 pub fn extern_fn(namespace: &Namespace, efn: &ExternFn) -> String {
-    let receiver_type = match &efn.receiver {
-        Some(receiver) => receiver.ident.to_string(),
-        None => "_".to_string(),
+    let receiver = match &efn.receiver {
+        Some(receiver) => receiver.ident.to_string() + "$",
+        None => String::new(),
     };
-    format!("{}cxxbridge02${}${}", namespace, receiver_type, efn.ident)
+    format!("{}cxxbridge02${}{}", namespace, receiver, efn.ident)
 }
