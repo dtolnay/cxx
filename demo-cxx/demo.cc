b89091da0b631624280123dbe9c43cfff6d56c6b
Philip Craig <689193+philipcraig@users.noreply.github.com>
2020-05-10 14:28:22 +0100

    use make_unique in cpp example
    As it's more vernacular due to exception safety (no naked news)

--- demo-cxx/demo.cc
+++ demo-cxx/demo.cc
@@ -10,7 +10,7 @@ ThingC::ThingC(std::string appname) : appname(std::move(appname)) {}
 ThingC::~ThingC() { std::cout << "done with ThingC" << std::endl; }
 
 std::unique_ptr<ThingC> make_demo(rust::Str appname) {
-  return std::unique_ptr<ThingC>(new ThingC(std::string(appname)));
+  return std::make_unique<ThingC>(std::string(appname));
 }
 
 const std::string &get_name(const ThingC &thing) { return thing.appname; }
