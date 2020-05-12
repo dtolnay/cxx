25965d1e60cf49aa899bbb164d95a3fe8c2eb5a9
Philip Craig <689193+philipcraig@users.noreply.github.com>
2020-05-12 23:33:09 +0100

    specify c++ standard flag for msvc

--- build.rs
+++ build.rs
@@ -1,15 +1,21 @@
 fn main() {
+    let flag_prefix = if cfg!(target_env = "msvc") {
+        "/std:"
+    } else {
+        "-std="
+    };
+    let cpp_version = if cfg!(feature = "c++14") {
+        "14"
+    } else if cfg!(feature = "c++17") {
+        "17"
+    } else {
+        "11"
+    };
     cc::Build::new()
         .file("src/cxx.cc")
         .cpp(true)
         .cpp_link_stdlib(None) // linked via link-cplusplus crate
-        .flag_if_supported(if cfg!(feature = "c++17") {
-            "-std=c++17"
-        } else if cfg!(feature = "c++14") {
-            "-std=c++14"
-        } else {
-            "-std=c++11"
-        })
+        .flag_if_supported(&format!("{}c++{}", flag_prefix, cpp_version))
         .compile("cxxbridge03");
     println!("cargo:rerun-if-changed=src/cxx.cc");
     println!("cargo:rerun-if-changed=include/cxx.h");
--- tests/ffi/build.rs
+++ tests/ffi/build.rs
@@ -3,9 +3,22 @@ fn main() {
         return;
     }
 
+    let flag_prefix = if cfg!(target_env = "msvc") {
+        "/std:"
+    } else {
+        "-std="
+    };
+    let cpp_version = if cfg!(feature = "c++14") {
+        "14"
+    } else if cfg!(feature = "c++17") {
+        "17"
+    } else {
+        "11"
+    };
     let sources = vec!["lib.rs", "module.rs"];
+    dbg!(format!("{}c++{}", flag_prefix, cpp_version));
     cxx_build::bridges(sources)
         .file("tests.cc")
-        .flag_if_supported("-std=c++11")
+        .flag_if_supported(&format!("{}c++{}", flag_prefix, cpp_version))
         .compile("cxx-test-suite");
 }
