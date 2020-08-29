de1cb777b2e4fb3f7b1255500baf7c09d2ef7a3e
David Tolnay <dtolnay@gmail.com>
2020-08-28 17:25:29 -0700

    Specify consistent c++ standard between cxx and cxx-test-suite

--- build.rs
+++ build.rs
@@ -3,13 +3,7 @@ fn main() {
         .file("src/cxx.cc")
         .cpp(true)
         .cpp_link_stdlib(None) // linked via link-cplusplus crate
-        .flag_if_supported(if cfg!(feature = "c++17") {
-            "-std=c++17"
-        } else if cfg!(feature = "c++14") {
-            "-std=c++14"
-        } else {
-            "-std=c++11"
-        })
+        .flag_if_supported(cxxbridge_flags::STD)
         .compile("cxxbridge03");
     println!("cargo:rerun-if-changed=src/cxx.cc");
     println!("cargo:rerun-if-changed=include/cxx.h");
--- tests/ffi/build.rs
+++ tests/ffi/build.rs
@@ -6,6 +6,6 @@ fn main() {
     let sources = vec!["lib.rs", "module.rs"];
     cxx_build::bridges(sources)
         .file("tests.cc")
-        .flag_if_supported("-std=c++11")
+        .flag_if_supported(cxxbridge_flags::STD)
         .compile("cxx-test-suite");
 }
