020c923e6ba251837e25b70d0e64982a11cfd526
David Tolnay <dtolnay@gmail.com>
2020-08-29 11:34:00 -0700

    Less brittle matching of cxx::bridge path

--- gen/src/find.rs
+++ gen/src/find.rs
@@ -1,6 +1,5 @@
 use crate::gen::{Error, Input, Result};
 use crate::syntax::namespace::Namespace;
-use quote::quote;
 use syn::{Attribute, File, Item};
 
 pub(super) fn find_bridge_mod(syntax: File) -> Result<Input> {
@@ -14,8 +13,8 @@ fn scan(items: Vec<Item>) -> Result<Option<Input>> {
     for item in items {
         if let Item::Mod(item) = item {
             for attr in &item.attrs {
-                let path = &attr.path;
-                if quote!(#path).to_string() == "cxx :: bridge" {
+                let path = &attr.path.segments;
+                if path.len() == 2 && path[0].ident == "cxx" && path[1].ident == "bridge" {
                     let module = match item.content {
                         Some(module) => module.1,
                         None => {
