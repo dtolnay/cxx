c8256aff88af893ecfdf3f224da47e3d1a2aaa76
David Tolnay <dtolnay@gmail.com>
2020-08-30 01:32:41 -0700

    Link to some context about autocxx

--- gen/lib/src/lib.rs
+++ gen/lib/src/lib.rs
@@ -1,6 +1,11 @@
 //! The CXX code generator for constructing and compiling C++ code.
 //!
-//! This is intended to be embedded into higher-level code generators.
+//! This is intended as a mechanism for embedding the `cxx` crate into
+//! higher-level code generators. See [dtolnay/cxx#235] and
+//! [https://github.com/google/autocxx].
+//!
+//! [dtolnay/cxx#235]: https://github.com/dtolnay/cxx/issues/235
+//! [https://github.com/google/autocxx]: https://github.com/google/autocxx
 
 #![allow(dead_code)]
 
