84f8cf973862311b293d540acc1bc3e74fb8ed15
David Tolnay <dtolnay@gmail.com>
2020-09-01 15:14:19 -0700

    Unindent target dir cargo logic

--- gen/build/src/cargo.rs
+++ gen/build/src/cargo.rs
@@ -4,28 +4,29 @@ use std::process::Command;
 use std::str;
 
 pub(crate) fn target_dir(out_dir: &Path) -> TargetDir {
-    (|| {
-        let cargo = option_env!("CARGO").unwrap_or("cargo");
-        let output = Command::new(cargo)
-            .current_dir(out_dir)
-            .arg("metadata")
-            .arg("--no-deps")
-            .arg("--format-version=1")
-            .output()
-            .ok()?;
+    try_target_dir(out_dir).map_or(TargetDir::Unknown, TargetDir::Path)
+}
 
-        // Cargo only outputs utf8 encoded JSON.
-        let mut metadata = str::from_utf8(&output.stdout).ok()?;
+fn try_target_dir(out_dir: &Path) -> Option<PathBuf> {
+    let cargo = option_env!("CARGO").unwrap_or("cargo");
+    let output = Command::new(cargo)
+        .current_dir(out_dir)
+        .arg("metadata")
+        .arg("--no-deps")
+        .arg("--format-version=1")
+        .output()
+        .ok()?;
 
-        let key_pattern = "\"target_directory\":";
-        let key_index = metadata.rfind(key_pattern)?;
-        metadata = &metadata[key_index + key_pattern.len()..];
-        let open_quote_index = metadata.find('"')?;
-        metadata = &metadata[open_quote_index + 1..];
-        let close_quote_index = metadata.find('"')?;
-        let string = &metadata[..close_quote_index];
-        let target_directory = string.replace("\\\\", "\\");
-        Some(TargetDir::Path(PathBuf::from(target_directory)))
-    })()
-    .unwrap_or(TargetDir::Unknown)
+    // Cargo only outputs utf8 encoded JSON.
+    let mut metadata = str::from_utf8(&output.stdout).ok()?;
+
+    let key_pattern = "\"target_directory\":";
+    let key_index = metadata.rfind(key_pattern)?;
+    metadata = &metadata[key_index + key_pattern.len()..];
+    let open_quote_index = metadata.find('"')?;
+    metadata = &metadata[open_quote_index + 1..];
+    let close_quote_index = metadata.find('"')?;
+    let string = &metadata[..close_quote_index];
+    let target_directory = string.replace("\\\\", "\\");
+    Some(PathBuf::from(target_directory))
 }
