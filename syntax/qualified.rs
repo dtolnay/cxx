6c9a622708a877257c030c679452b716c6a7a1e5
David Tolnay <dtolnay@gmail.com>
2020-09-06 23:08:57 -0700

    Permit rust keywords in C++ qualified paths

--- syntax/qualified.rs
+++ syntax/qualified.rs
@@ -1,5 +1,6 @@
+use syn::ext::IdentExt;
 use syn::parse::{ParseStream, Result};
-use syn::{Ident, Path};
+use syn::{Ident, Token};
 
 pub struct QualifiedName {
     pub segments: Vec<Ident>,
@@ -7,10 +8,18 @@ pub struct QualifiedName {
 
 impl QualifiedName {
     pub fn parse_unquoted(input: ParseStream) -> Result<Self> {
-        let path = input.call(Path::parse_mod_style)?;
-        let mut segments = Vec::with_capacity(path.segments.len());
-        for segment in path.segments {
-            segments.push(segment.ident);
+        let mut segments = Vec::new();
+        let mut trailing_punct = true;
+        while trailing_punct && input.peek(Ident::peek_any) {
+            let ident = Ident::parse_any(input)?;
+            segments.push(ident);
+            let colons: Option<Token![::]> = input.parse()?;
+            trailing_punct = colons.is_some();
+        }
+        if segments.is_empty() {
+            return Err(input.error("expected path"));
+        } else if trailing_punct {
+            return Err(input.error("expected path segment"));
         }
         Ok(QualifiedName { segments })
     }
