b780338925ceed2c1da3d85473e42edc4fb9f46a
Roland Fischer <roli@gugus.ca>
2020-09-25 14:17:39 -0400

    Set C++ std in Cargo.toml for demo

--- demo/build.rs
+++ demo/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     cxx_build::bridge("src/main.rs")
         .file("src/demo.cc")
-        .flag_if_supported("-std=c++14")
+        .flag_if_supported(cxxbridge_flags::STD)
         .compile("cxxbridge-demo");
 
     println!("cargo:rerun-if-changed=src/main.rs");
