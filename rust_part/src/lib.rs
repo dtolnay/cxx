5ca288b5855526fd9256b4359f778a68d5015a8a
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2020-10-29 10:46:00 +0800

    bump ubuntu version in ci

--- rust_part/src/lib.rs
+++ rust_part/src/lib.rs
@@ -1,9 +1,17 @@
+use std::fmt;
+
 #[cxx::bridge(namespace = rust_part)]
 mod ffi {
+    struct Color {
+        r: u8,
+        g: u8,
+        b: u8,
+    }
 
     struct SharedThing {
         points: Box<Points>,
         persons: UniquePtr<Person>,
+        pixels: Vec<Color>,
     }
 
     extern "C++" {
@@ -28,8 +36,29 @@ pub struct Points {
     y: Vec<u8>,
 }
 
+impl ffi::Color {
+    pub fn new() -> Self {
+        Self {
+            r: 255,
+            g: 255,
+            b: 255,
+        }
+    }
+}
+
+impl fmt::Debug for ffi::Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Color")
+            .field("r", &self.r)
+            .field("g", &self.g)
+            .field("b", &self.b)
+            .finish()
+    }
+}
+
 fn print_shared_thing(thing: &ffi::SharedThing) {
     println!("{:#?}", thing.points);
+    println!("{:#?}", thing.pixels);
     println!("{:#?}", ffi::get_name(thing.persons.as_ref().unwrap()));
 }
 
@@ -40,6 +69,7 @@ fn make_shared_thing() -> ffi::SharedThing {
             y: vec![4, 5, 6],
         }),
         persons: ffi::make_person(),
+        pixels: vec![ffi::Color::new(), ffi::Color::new()],
     }
 }
 
