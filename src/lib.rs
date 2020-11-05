ce5b068efa60c993baca086007b09fa27423ffce
David Tolnay <dtolnay@gmail.com>
2020-11-04 16:32:04 -0800

    Release 0.5.4

--- src/lib.rs
+++ src/lib.rs
@@ -349,7 +349,7 @@
 //! [https://github.com/dtolnay/cxx]: https://github.com/dtolnay/cxx
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/cxx/0.5.3")]
+#![doc(html_root_url = "https://docs.rs/cxx/0.5.4")]
 #![deny(improper_ctypes)]
 #![allow(non_camel_case_types)]
 #![allow(
