131a42c5e5d77e69b7e175dc956692a09061557c
david-cattermole <cattermole91@gmail.com>
2020-11-08 15:28:59 +1100

    Do not use std::make_unique, because it's not in C++11.
    std::make_unique is only in C++14 and above.

--- include/mmscenegraph.h
+++ include/mmscenegraph.h
@@ -1,27 +1,6 @@
 #ifndef MMSCENEGRAPH_H
 #define MMSCENEGRAPH_H
 
-// On MSVC, <memory> will bring in the TEMPLATE macros needed
-#include <memory> 
-
-// Create create a 'make_unique' function because it's missing in older
-// versions of GCC and MSVC.
-//
-// https://herbsutter.com/gotw/_102/
-#ifdef __GNUC__
-#if __cplusplus < 201402L  // Less than C++14
-
-namespace std {
-    template<typename T, typename... Args>
-    std::unique_ptr<T> make_unique(Args&&... args)
-    {
-        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-    }
-} // std namespace
-
-#endif  // GCC_VERSION
-#endif  // __GNUC__
-
 #include <cxx.h>
 #include <mmscenegraph/_cbindgen.h>
 #include <mmscenegraph/_cxxbridge.h>
