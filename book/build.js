8f8ee538af8fc57400cae996609b91c8f3d141db
David Tolnay <dtolnay@gmail.com>
2020-11-16 20:18:06 -0800

    Book style tweaks

--- book/build.js
+++ book/build.js
@@ -14,6 +14,11 @@ https://github.com/dtolnay/cxx\
 </a>\
 </li>`;
 
+const htmljs = `\
+var html = document.querySelector('html');
+html.classList.remove('no-js');
+html.classList.add('js');`;
+
 const dirs = ['build'];
 while (dirs.length) {
   const dir = dirs.pop();
@@ -32,7 +37,11 @@ while (dirs.length) {
     const index = fs.readFileSync(path, 'utf8');
     const $ = cheerio.load(index, { decodeEntities: false });
 
+    $('script:nth-of-type(3)').text(htmljs);
     $('nav#sidebar ol.chapter').append(githublink);
+    $('head link[href="tomorrow-night.css"]').attr('disabled', true);
+    $('head link[href="ayu-highlight.css"]').attr('disabled', true);
+    $('button#theme-toggle').attr('style', 'display:none');
     $('pre code').each(function () {
       const node = $(this);
       const langClass = node.attr('class').split(' ', 2)[0];
@@ -44,6 +53,7 @@ while (dirs.length) {
       const boring = lines.map((line) =>
         line.includes('<span class="boring">')
       );
+      const ellipsis = lines.map((line) => line.includes('// ...'));
       const target = entities.decode(node.text());
       const highlighted = hljs.highlight(lang, target).value;
       const result = highlighted
@@ -51,14 +61,21 @@ while (dirs.length) {
         .map(function (line, i) {
           if (boring[i]) {
             line = '<span class="boring">' + line;
+          } else if (ellipsis[i]) {
+            line = '<span class="ellipsis">' + line;
           }
-          if (i > 0 && boring[i - 1]) {
+          if (i > 0 && (boring[i - 1] || ellipsis[i - 1])) {
             line = '</span>' + line;
           }
           return line;
         })
         .join('\n');
-      node.text(result).removeClass(langClass).addClass('hidelines');
+      node.text(result);
+      node.removeClass(langClass);
+      if (!node.hasClass('focuscomment')) {
+        node.addClass('hidelines');
+        node.addClass('hide-boring');
+      }
     });
     $('code').each(function () {
       $(this).addClass('hljs');
@@ -68,3 +85,10 @@ while (dirs.length) {
     fs.writeFileSync(path, out);
   });
 }
+
+fs.copyFileSync('build/highlight.css', 'build/tomorrow-night.css');
+fs.copyFileSync('build/highlight.css', 'build/ayu-highlight.css');
+
+var bookjs = fs.readFileSync('build/book.js', 'utf8');
+bookjs = bookjs.replace('set_theme(theme, false);', '');
+fs.writeFileSync('build/book.js', bookjs);
