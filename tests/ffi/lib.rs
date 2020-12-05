99b3cb7bc494ffa172e80416c23d3983621df7ff
David Tolnay <dtolnay@gmail.com>
2020-12-04 17:27:40 -0800

    Add test of returning Vec with struct from other bridge

--- tests/ffi/lib.rs
+++ tests/ffi/lib.rs
@@ -184,7 +184,10 @@ pub mod ffi {
     }
 
     extern "C++" {
+        include!("tests/ffi/module.rs.h");
+
         type COwnedEnum;
+        type Job = crate::module::ffi::Job;
     }
 
     #[repr(u32)]
@@ -211,6 +214,7 @@ pub mod ffi {
         fn r_return_unique_ptr_string() -> UniquePtr<CxxString>;
         fn r_return_rust_vec() -> Vec<u8>;
         fn r_return_rust_vec_string() -> Vec<String>;
+        fn r_return_rust_vec_extern_struct() -> Vec<Job>;
         fn r_return_ref_rust_vec(shared: &Shared) -> &Vec<u8>;
         fn r_return_mut_rust_vec(shared: &mut Shared) -> &mut Vec<u8>;
         fn r_return_identity(_: usize) -> usize;
@@ -432,6 +436,10 @@ fn r_return_rust_vec() -> Vec<u8> {
 }
 
 fn r_return_rust_vec_string() -> Vec<String> {
+    Vec::new()
+}
+
+fn r_return_rust_vec_extern_struct() -> Vec<ffi::Job> {
     Vec::new()
 }
 
