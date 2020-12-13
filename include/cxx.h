1e784a3487190dcd5337b3d104f8c5de1c8ee760
David Tolnay <dtolnay@gmail.com>
2020-12-12 21:34:47 -0800

    Add aliases for the Rust numeric primitives

--- include/cxx.h
+++ include/cxx.h
@@ -417,6 +417,18 @@ public:
 template <typename T>
 struct IsRelocatable;
 
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t; // see static asserts in cxx.cc
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using f32 = float;
+using f64 = double;
+
 // Snake case aliases for use in code that uses this style for type names.
 using string = String;
 using str = Str;
