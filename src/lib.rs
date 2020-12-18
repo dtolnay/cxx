3077818a1fa82abd2838adb13abfe0cdcc0f9499
David Tolnay <dtolnay@gmail.com>
2020-12-18 11:27:27 -0800

    Release 1.0.19

--- src/lib.rs
+++ src/lib.rs
@@ -363,7 +363,7 @@
 //! </table>
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/cxx/1.0.18")]
+#![doc(html_root_url = "https://docs.rs/cxx/1.0.19")]
 #![deny(improper_ctypes)]
 #![allow(non_camel_case_types)]
 #![allow(
