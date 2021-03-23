c37a665e042077925871dfd1206f410a570e1068
David Tolnay <dtolnay@gmail.com>
2021-03-22 20:19:09 -0700

    Release 1.0.34

--- src/lib.rs
+++ src/lib.rs
@@ -364,7 +364,7 @@
 //! </table>
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/cxx/1.0.33")]
+#![doc(html_root_url = "https://docs.rs/cxx/1.0.34")]
 #![deny(improper_ctypes)]
 #![allow(non_camel_case_types)]
 #![allow(
