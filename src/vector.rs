676bb463840c9d5bef87cdd7fe99528d23ec603d
David Tolnay <dtolnay@gmail.com>
2021-03-25 01:05:01 -0400

    Make rustdoc Re-exports section show external-facing type name

--- src/vector.rs
+++ src/vector.rs
@@ -2,7 +2,7 @@
 //! itself is exposed at the crate root.
 
 pub use crate::cxx_vector::{Iter, IterMut};
-#[doc(no_inline)]
-pub use crate::CxxVector;
 #[doc(inline)]
 pub use crate::Vector;
+#[doc(no_inline)]
+pub use cxx::CxxVector;
