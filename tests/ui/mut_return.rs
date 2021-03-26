944521ad0f062aefeb8e9445c7b0c2089d99a4a9
David Tolnay <dtolnay@gmail.com>
2021-03-26 17:57:49 -0400

    Add some more mut return ui tests

--- tests/ui/mut_return.rs
+++ tests/ui/mut_return.rs
@@ -1,10 +1,16 @@
 #[cxx::bridge]
 mod ffi {
+    extern "Rust" {
+        type Mut<'a>;
+    }
+
     unsafe extern "C++" {
         type Thing;
 
         fn f(t: &Thing) -> Pin<&mut CxxString>;
         unsafe fn g(t: &Thing) -> Pin<&mut CxxString>;
+        fn h(t: Box<Mut>) -> Pin<&mut CxxString>;
+        fn i<'a>(t: Box<Mut<'a>>) -> Pin<&'a mut CxxString>;
     }
 }
 
