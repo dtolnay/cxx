5577e7c320274b5a1b8bf074d83ae31dcc0dd091
David Tolnay <dtolnay@gmail.com>
2021-04-10 11:36:45 -0700

    Encapsulate RustSlice repr to single module

--- src/rust_slice.rs
+++ src/rust_slice.rs
@@ -4,34 +4,54 @@ use core::slice;
 
 #[repr(C)]
 pub struct RustSlice {
-    pub(crate) repr: NonNull<[()]>,
+    repr: NonNull<[()]>,
 }
 
 impl RustSlice {
     pub fn from_ref<T>(slice: &[T]) -> Self {
-        let ptr = ptr::slice_from_raw_parts::<()>(slice.as_ptr().cast(), slice.len());
-        RustSlice {
-            repr: unsafe { NonNull::new_unchecked(ptr as *mut _) },
-        }
+        let ptr = NonNull::from(slice).cast::<T>();
+        let len = slice.len();
+        Self::from_raw_parts(ptr, len)
     }
 
     pub fn from_mut<T>(slice: &mut [T]) -> Self {
-        let ptr = ptr::slice_from_raw_parts_mut(slice.as_mut_ptr().cast(), slice.len());
+        let ptr = NonNull::from(&mut *slice).cast::<T>();
+        let len = slice.len();
+        Self::from_raw_parts(ptr, len)
+    }
+
+    pub unsafe fn as_slice<'a, T>(self) -> &'a [T] {
+        let ptr = self.as_ptr();
+        let len = self.len();
+        slice::from_raw_parts(ptr.as_ptr(), len)
+    }
+
+    pub unsafe fn as_mut_slice<'a, T>(self) -> &'a mut [T] {
+        let ptr = self.as_ptr();
+        let len = self.len();
+        slice::from_raw_parts_mut(ptr.as_ptr(), len)
+    }
+
+    pub(crate) fn from_raw_parts<T>(ptr: NonNull<T>, len: usize) -> Self {
+        // TODO: use NonNull::from_raw_parts(ptr.cast(), len) when stable.
+        // https://doc.rust-lang.org/nightly/std/ptr/struct.NonNull.html#method.from_raw_parts
+        // https://github.com/rust-lang/rust/issues/81513
+        let ptr = ptr::slice_from_raw_parts_mut(ptr.as_ptr().cast(), len);
         RustSlice {
             repr: unsafe { NonNull::new_unchecked(ptr) },
         }
     }
 
-    pub unsafe fn as_slice<'a, T>(self) -> &'a [T] {
-        let ptr = self.repr.as_ptr();
-        let len = self.repr.as_ref().len();
-        slice::from_raw_parts(ptr.cast(), len)
+    pub(crate) fn as_ptr<T>(&self) -> NonNull<T> {
+        self.repr.cast()
     }
 
-    pub unsafe fn as_mut_slice<'a, T>(self) -> &'a mut [T] {
-        let ptr = self.repr.as_ptr();
-        let len = self.repr.as_ref().len();
-        slice::from_raw_parts_mut(ptr.cast(), len)
+    pub(crate) fn len(&self) -> usize {
+        // TODO: use self.repr.len() when stable.
+        // https://doc.rust-lang.org/nightly/std/ptr/struct.NonNull.html#method.len
+        // https://github.com/rust-lang/rust/issues/71146
+        let slice = unsafe { self.repr.as_ref() };
+        slice.len()
     }
 }
 
--- src/symbols/rust_slice.rs
+++ src/symbols/rust_slice.rs
@@ -3,20 +3,17 @@ use core::mem::MaybeUninit;
 use core::ptr::{self, NonNull};
 
 #[export_name = "cxxbridge1$slice$new"]
-unsafe extern "C" fn slice_new(this: &mut MaybeUninit<RustSlice>, ptr: *const (), len: usize) {
-    let ptr = ptr::slice_from_raw_parts(ptr, len);
-    let rust_slice = RustSlice {
-        repr: NonNull::new_unchecked(ptr as *mut _),
-    };
+unsafe extern "C" fn slice_new(this: &mut MaybeUninit<RustSlice>, ptr: NonNull<()>, len: usize) {
+    let rust_slice = RustSlice::from_raw_parts(ptr, len);
     ptr::write(this.as_mut_ptr(), rust_slice);
 }
 
 #[export_name = "cxxbridge1$slice$ptr"]
-unsafe extern "C" fn slice_ptr(this: &RustSlice) -> *const () {
-    this.repr.as_ptr().cast()
+unsafe extern "C" fn slice_ptr(this: &RustSlice) -> NonNull<()> {
+    this.as_ptr()
 }
 
 #[export_name = "cxxbridge1$slice$len"]
 unsafe extern "C" fn slice_len(this: &RustSlice) -> usize {
-    this.repr.as_ref().len()
+    this.len()
 }
