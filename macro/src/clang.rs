cacfa023a4884690091104e784cfac190c5755fb
David Tolnay <dtolnay@gmail.com>
2021-04-22 13:02:57 -0700

    JSON deserialization based on CXX_CLANG_AST env var

--- macro/src/clang.rs
+++ macro/src/clang.rs
@@ -1,6 +1,11 @@
 use crate::syntax::report::Errors;
 use crate::syntax::Api;
 use serde::Deserialize;
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+const CXX_CLANG_AST: &str = "CXX_CLANG_AST";
 
 type Node = clang_ast::Node<Clang>;
 
@@ -19,10 +24,37 @@ pub fn load(cx: &mut Errors, apis: &mut [Api]) {
         }
     }
 
-    if variants_from_header.is_empty() {
-        return;
-    }
+    let span = match variants_from_header.get(0) {
+        None => return,
+        Some(enm) => enm.variants_from_header_attr.clone().unwrap(),
+    };
 
-    let _ = cx;
+    let ast_dump_path = match env::var_os(CXX_CLANG_AST) {
+        Some(ast_dump_path) => PathBuf::from(ast_dump_path),
+        None => {
+            let msg = format!(
+                "environment variable ${} has not been provided",
+                CXX_CLANG_AST,
+            );
+            return cx.error(span, msg);
+        }
+    };
+
+    let ast_dump_bytes = match fs::read(&ast_dump_path) {
+        Ok(ast_dump_bytes) => ast_dump_bytes,
+        Err(error) => {
+            let msg = format!("failed to read {}: {}", ast_dump_path.display(), error);
+            return cx.error(span, msg);
+        }
+    };
+
+    let root: Node = match serde_json::from_slice(&ast_dump_bytes) {
+        Ok(root) => root,
+        Err(error) => {
+            let msg = format!("failed to read {}: {}", ast_dump_path.display(), error);
+            return cx.error(span, msg);
+        }
+    };
+
     unimplemented!()
 }
