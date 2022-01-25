1c5edfed9b13f599b226c31944c2f193e5a78562
Russell Greene <russellgreene8@gmail.com>
2022-01-25 15:02:49 -0600

    Check contents of moved string

--- tests/test.rs
+++ tests/test.rs
@@ -292,6 +292,7 @@ fn test_unique_to_shared_ptr() {
     let ptr = &*unique as *const _;
     let shared = unique.to_shared();
     assert_eq!(&*shared as *const _, ptr);
+    assert_eq!(&*shared, "2020");
 }
 
 #[test]
