cea0cef207d400d5222a224bff9026fcbb949019
David Tolnay <dtolnay@gmail.com>
2023-03-17 18:48:01 -0700

    Release 1.0.93

--- src/lib.rs
+++ src/lib.rs
@@ -364,7 +364,7 @@
 //! </table>
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/cxx/1.0.92")]
+#![doc(html_root_url = "https://docs.rs/cxx/1.0.93")]
 #![deny(
     improper_ctypes,
     improper_ctypes_definitions,
