47e3f0b1a86388696ebc2166ace56f4367eacea1
David Tolnay <dtolnay@gmail.com>
2023-05-27 11:01:21 -0700

    Fix typo in comment in SharedPtr implementation

--- src/shared_ptr.rs
+++ src/shared_ptr.rs
@@ -191,8 +191,8 @@ pub unsafe trait SharedPtrTarget {
     where
         Self: Sized,
     {
-        // Opoaque C types do not get this method because they can never exist
-        // by value on the Rust side of the bridge.
+        // Opaque C types do not get this method because they can never exist by
+        // value on the Rust side of the bridge.
         let _ = value;
         let _ = new;
         unreachable!()
