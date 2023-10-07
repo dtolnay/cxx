7cc26d0a6f81e165585687c75ba1db5a398ff611
David Tolnay <dtolnay@gmail.com>
2023-10-06 22:27:51 -0400

    Clippy incorrect_partial_ord_impl_on_ord_type lint has been renamed
        warning: lint `clippy::incorrect_partial_ord_impl_on_ord_type` has been renamed to `clippy::non_canonical_partial_ord_impl`
          --> tests/ffi/lib.rs:27:43
           |
        27 |     #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
           |                                           ^^^^^^^^^^ help: use the new name: `clippy::non_canonical_partial_ord_impl`
           |
           = note: `#[warn(renamed_and_removed_lints)]` on by default
    
        warning: lint `clippy::incorrect_partial_ord_impl_on_ord_type` has been renamed to `clippy::non_canonical_partial_ord_impl`
          --> tests/ffi/lib.rs:32:25
           |
        32 |     #[derive(PartialEq, PartialOrd)]
           |                         ^^^^^^^^^^ help: use the new name: `clippy::non_canonical_partial_ord_impl`
    
        warning: lint `clippy::incorrect_partial_ord_impl_on_ord_type` has been renamed to `clippy::non_canonical_partial_ord_impl`
          --> tests/ffi/lib.rs:37:27
           |
        37 |     #[derive(Debug, Hash, PartialOrd, Ord)]
           |                           ^^^^^^^^^^ help: use the new name: `clippy::non_canonical_partial_ord_impl`
    
        warning: lint `clippy::incorrect_partial_ord_impl_on_ord_type` has been renamed to `clippy::non_canonical_partial_ord_impl`
          --> tests/ffi/lib.rs:89:69
           |
        89 |     #[derive(Copy, Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
           |                                                                     ^^^^^^^^^^ help: use the new name: `clippy::non_canonical_partial_ord_impl`

--- macro/src/derive.rs
+++ macro/src/derive.rs
@@ -215,7 +215,7 @@ fn struct_partial_ord(strct: &Struct, span: Span) -> TokenStream {
 
     quote_spanned! {span=>
         impl #generics ::cxx::core::cmp::PartialOrd for #ident #generics {
-            #[allow(clippy::incorrect_partial_ord_impl_on_ord_type)]
+            #[allow(clippy::non_canonical_partial_ord_impl)]
             fn partial_cmp(&self, other: &Self) -> ::cxx::core::option::Option<::cxx::core::cmp::Ordering> {
                 #body
             }
@@ -284,7 +284,7 @@ fn enum_partial_ord(enm: &Enum, span: Span) -> TokenStream {
 
     quote_spanned! {span=>
         impl ::cxx::core::cmp::PartialOrd for #ident {
-            #[allow(clippy::incorrect_partial_ord_impl_on_ord_type)]
+            #[allow(clippy::non_canonical_partial_ord_impl)]
             fn partial_cmp(&self, other: &Self) -> ::cxx::core::option::Option<::cxx::core::cmp::Ordering> {
                 ::cxx::core::cmp::PartialOrd::partial_cmp(&self.repr, &other.repr)
             }
