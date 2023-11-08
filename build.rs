cff0928dbcd700f8fc101784899583f54adb3c12
Adrian Taylor <adetaylor@chromium.org>
2023-11-08 17:49:29 +0000

    Provide cfg option to disable use of cc.
    In the Chromium build system, we automatically convert Cargo.toml rules into
    rules for our build system, gn. We also run build.rs files as part of the
    Chromium build (because it turns out that the vast majority of them just set
    some feature flags for feature detection).
    
    The build.rs for cxx does more sophisticated things - including building C++
    code using cc. That's appropriate in a cargo context, but presents difficulty
    for our build system where C++ must be built using static build rules beyond the
    realm of cargo. At the moment we carry a patch for this, but it would be great
    not to have to do so.
    
    This patch would enable us to use the Cargo.toml and build.rs for cxx without
    change. It's only one out of four patches we currently carry for cxx, but it
    would be great to get rid of it! Obviously this may seem like a bit of a niche
    use-case, but hopefully this helps anyone else who builds cxx using non-Cargo
    build systems, but reusing the Cargo.toml and build.rs.

--- build.rs
+++ build.rs
@@ -1,27 +1,11 @@
 use std::env;
-use std::path::{Path, PathBuf};
 use std::process::Command;
+#[cfg(feature="cc")]
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let manifest_dir_opt = env::var_os("CARGO_MANIFEST_DIR").map(PathBuf::from);
-    let manifest_dir = manifest_dir_opt.as_deref().unwrap_or(Path::new(""));
-
-    cc::Build::new()
-        .file(manifest_dir.join("src/cxx.cc"))
-        .cpp(true)
-        .cpp_link_stdlib(None) // linked via link-cplusplus crate
-        .flag_if_supported(cxxbridge_flags::STD)
-        .warnings_into_errors(cfg!(deny_warnings))
-        .compile("cxxbridge1");
-
-    println!("cargo:rerun-if-changed=src/cxx.cc");
-    println!("cargo:rerun-if-changed=include/cxx.h");
-    println!("cargo:rustc-cfg=built_with_cargo");
-
-    if let Some(manifest_dir) = &manifest_dir_opt {
-        let cxx_h = manifest_dir.join("include").join("cxx.h");
-        println!("cargo:HEADER={}", cxx_h.to_string_lossy());
-    }
+    #[cfg(feature="cc")]
+    build_cc();
 
     if let Some(rustc) = rustc_version() {
         if rustc.minor < 60 {
@@ -49,4 +33,27 @@ fn rustc_version() -> Option<RustVersion> {
     }
     let minor = pieces.next()?.parse().ok()?;
     Some(RustVersion { version, minor })
+}
+
+#[cfg(feature="cc")]
+fn build_cc() {
+    let manifest_dir_opt = env::var_os("CARGO_MANIFEST_DIR").map(PathBuf::from);
+    let manifest_dir = manifest_dir_opt.as_deref().unwrap_or(Path::new(""));
+
+    cc::Build::new()
+        .file(manifest_dir.join("src/cxx.cc"))
+        .cpp(true)
+        .cpp_link_stdlib(None) // linked via link-cplusplus crate
+        .flag_if_supported(cxxbridge_flags::STD)
+        .warnings_into_errors(cfg!(deny_warnings))
+        .compile("cxxbridge1");
+
+    println!("cargo:rerun-if-changed=src/cxx.cc");
+    println!("cargo:rerun-if-changed=include/cxx.h");
+    println!("cargo:rustc-cfg=built_with_cargo");
+
+    if let Some(manifest_dir) = &manifest_dir_opt {
+        let cxx_h = manifest_dir.join("include").join("cxx.h");
+        println!("cargo:HEADER={}", cxx_h.to_string_lossy());
+    }
 }
