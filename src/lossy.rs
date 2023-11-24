5289ce332cde36d31c54faba42cd64a148c3f94f
David Tolnay <dtolnay@gmail.com>
2023-11-23 19:45:00 -0800

    Utf8Chunks with next_valid / next_invalid

--- src/lossy.rs
+++ src/lossy.rs
@@ -1,38 +1,70 @@
+use self::utf8_chunks::Utf8Chunks;
 use core::char;
 use core::fmt::{self, Write as _};
-use core::str;
 
-pub(crate) fn display(mut bytes: &[u8], f: &mut fmt::Formatter) -> fmt::Result {
-    loop {
-        match str::from_utf8(bytes) {
-            Ok(valid) => return f.write_str(valid),
-            Err(utf8_error) => {
-                let valid_up_to = utf8_error.valid_up_to();
-                let valid = unsafe { str::from_utf8_unchecked(&bytes[..valid_up_to]) };
-                f.write_str(valid)?;
-                f.write_char(char::REPLACEMENT_CHARACTER)?;
-                if let Some(error_len) = utf8_error.error_len() {
-                    bytes = &bytes[valid_up_to + error_len..];
-                } else {
-                    return Ok(());
+mod utf8_chunks {
+    use core::str;
+
+    pub struct Utf8Chunks<'a> {
+        rest: &'a [u8],
+    }
+
+    impl<'a> Utf8Chunks<'a> {
+        pub fn new(bytes: &'a [u8]) -> Self {
+            Utf8Chunks { rest: bytes }
+        }
+
+        pub fn is_empty(&self) -> bool {
+            self.rest.is_empty()
+        }
+
+        pub fn next_valid(&mut self) -> &'a str {
+            let valid = match str::from_utf8(self.rest) {
+                Ok(valid) => valid,
+                Err(utf8_error) => {
+                    let valid_up_to = utf8_error.valid_up_to();
+                    unsafe { str::from_utf8_unchecked(&self.rest[..valid_up_to]) }
                 }
-            }
+            };
+            self.rest = &self.rest[valid.len()..];
+            valid
+        }
+
+        pub fn next_invalid(&mut self) -> &'a [u8] {
+            let invalid = if let Err(utf8_error) = str::from_utf8(self.rest) {
+                if utf8_error.valid_up_to() > 0 {
+                    &[]
+                } else if let Some(error_len) = utf8_error.error_len() {
+                    &self.rest[..error_len]
+                } else {
+                    self.rest
+                }
+            } else {
+                &[]
+            };
+            self.rest = &self.rest[invalid.len()..];
+            invalid
         }
     }
 }
 
-pub(crate) fn debug(mut bytes: &[u8], f: &mut fmt::Formatter) -> fmt::Result {
+pub(crate) fn display(bytes: &[u8], f: &mut fmt::Formatter) -> fmt::Result {
+    let mut chunks = Utf8Chunks::new(bytes);
+    while !chunks.is_empty() {
+        f.write_str(chunks.next_valid())?;
+        if !chunks.next_invalid().is_empty() {
+            f.write_char(char::REPLACEMENT_CHARACTER)?;
+        }
+    }
+    Ok(())
+}
+
+pub(crate) fn debug(bytes: &[u8], f: &mut fmt::Formatter) -> fmt::Result {
     f.write_char('"')?;
 
-    while !bytes.is_empty() {
-        let from_utf8_result = str::from_utf8(bytes);
-        let valid = match from_utf8_result {
-            Ok(valid) => valid,
-            Err(utf8_error) => {
-                let valid_up_to = utf8_error.valid_up_to();
-                unsafe { str::from_utf8_unchecked(&bytes[..valid_up_to]) }
-            }
-        };
+    let mut chunks = Utf8Chunks::new(bytes);
+    while !chunks.is_empty() {
+        let valid = chunks.next_valid();
 
         let mut written = 0;
         for (i, ch) in valid.char_indices() {
@@ -47,19 +79,8 @@ pub(crate) fn debug(mut bytes: &[u8], f: &mut fmt::Formatter) -> fmt::Result {
         }
         f.write_str(&valid[written..])?;
 
-        match from_utf8_result {
-            Ok(_valid) => break,
-            Err(utf8_error) => {
-                let end_of_broken = if let Some(error_len) = utf8_error.error_len() {
-                    valid.len() + error_len
-                } else {
-                    bytes.len()
-                };
-                for b in &bytes[valid.len()..end_of_broken] {
-                    write!(f, "\\x{:02x}", b)?;
-                }
-                bytes = &bytes[end_of_broken..];
-            }
+        for b in chunks.next_invalid() {
+            write!(f, "\\x{:02x}", b)?;
         }
     }
 
