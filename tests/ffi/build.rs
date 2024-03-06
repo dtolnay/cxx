fe8fdcad16068c94be86b414bec9be179771c769
David Tolnay <dtolnay@gmail.com>
2024-03-05 23:54:41 -0800

    Set C++ standard version using cc::Build::std

--- tests/ffi/build.rs
+++ tests/ffi/build.rs
@@ -9,7 +9,7 @@ fn main() {
     let sources = vec!["lib.rs", "module.rs"];
     let mut build = cxx_build::bridges(sources);
     build.file("tests.cc");
-    build.flag_if_supported(cxxbridge_flags::STD);
+    build.std(cxxbridge_flags::STD);
     build.warnings_into_errors(cfg!(deny_warnings));
     if cfg!(not(target_env = "msvc")) {
         build.define("CXX_TEST_INSTANTIATIONS", None);
