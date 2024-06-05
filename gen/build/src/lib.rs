bdb44f4dc04ae2d9f24cbdf5b630e3d2267a0653
David Tolnay <dtolnay@gmail.com>
2024-06-04 23:17:26 -0700

    Release 1.0.123

--- gen/build/src/lib.rs
+++ gen/build/src/lib.rs
@@ -45,7 +45,7 @@
 //! $ cxxbridge src/main.rs > path/to/mybridge.cc
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/cxx-build/1.0.122")]
+#![doc(html_root_url = "https://docs.rs/cxx-build/1.0.123")]
 #![cfg_attr(not(check_cfg), allow(unexpected_cfgs))]
 #![allow(
     clippy::cast_sign_loss,
--- src/lib.rs
+++ src/lib.rs
@@ -364,7 +364,7 @@
 //! </table>
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/cxx/1.0.122")]
+#![doc(html_root_url = "https://docs.rs/cxx/1.0.123")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(
     improper_ctypes,
