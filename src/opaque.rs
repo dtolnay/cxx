e19c267ee02ae815790919665d6f08b5731fa9ac
David Tolnay <dtolnay@gmail.com>
2024-08-30 12:51:51 -0700

    Reduce verbose !Sync opaque type errors

--- src/opaque.rs
+++ src/opaque.rs
@@ -15,8 +15,14 @@ use core::mem;
 pub struct Opaque {
     _private: [*const void; 0],
     _pinned: PhantomData<PhantomPinned>,
-    _mutable: UnsafeCell<()>,
+    _mutable: SyncUnsafeCell<()>,
 }
+
+// TODO: https://github.com/rust-lang/rust/issues/95439
+#[repr(transparent)]
+struct SyncUnsafeCell<T>(UnsafeCell<T>);
+
+unsafe impl<T> Sync for SyncUnsafeCell<T> {}
 
 const_assert_eq!(0, mem::size_of::<Opaque>());
 const_assert_eq!(1, mem::align_of::<Opaque>());
