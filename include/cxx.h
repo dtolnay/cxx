148458202fb061ac3a2f11714c4e4e59a1601777
David Tolnay <dtolnay@gmail.com>
2025-02-09 09:58:29 -0800

    Format PR 1437 with clang-format

--- include/cxx.h
+++ include/cxx.h
@@ -46,8 +46,9 @@ public:
   String(const char16_t *);
   String(const char16_t *, std::size_t);
 #if __cplusplus >= 202002L
-  String(const char8_t *s) : String(reinterpret_cast<const char*>(s)) {}
-  String(const char8_t *s, std::size_t len) : String(reinterpret_cast<const char*>(s), len) {}
+  String(const char8_t *s) : String(reinterpret_cast<const char *>(s)) {}
+  String(const char8_t *s, std::size_t len)
+      : String(reinterpret_cast<const char *>(s), len) {}
 #endif
 
   // Replace invalid Unicode data with the replacement character (U+FFFD).
