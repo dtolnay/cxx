dfb54b4a6b2c60de54431ae97c2014aee96c162c
David Tolnay <dtolnay@gmail.com>
2025-03-30 13:11:14 -0700

    Release 1.0.153

--- gen/build/src/lib.rs
+++ gen/build/src/lib.rs
@@ -45,7 +45,7 @@
 //! $ cxxbridge src/main.rs > path/to/mybridge.cc
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/cxx-build/1.0.152")]
+#![doc(html_root_url = "https://docs.rs/cxx-build/1.0.153")]
 #![cfg_attr(not(check_cfg), allow(unexpected_cfgs))]
 #![allow(
     clippy::cast_sign_loss,
