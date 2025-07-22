e197216329dd49930eff8c24b509885307f07b48
Lukasz Anforowicz <lukasza@chromium.org>
2025-07-22 23:41:50 +0000

    Extract `is_implicit_impl_ok` from `Types::collect` into a method of `ImplKey`.

--- syntax/instantiate.rs
+++ syntax/instantiate.rs
@@ -1,3 +1,4 @@
+use crate::syntax::types::Types;
 use crate::syntax::{NamedType, Ty1, Type};
 use proc_macro2::{Ident, Span};
 use std::hash::{Hash, Hasher};
@@ -11,6 +12,36 @@ pub(crate) enum ImplKey<'a> {
     SharedPtr(NamedImplKey<'a>),
     WeakPtr(NamedImplKey<'a>),
     CxxVector(NamedImplKey<'a>),
+}
+
+impl<'a> ImplKey<'a> {
+    /// Whether to generate an implicit instantiation/monomorphization of a given generic type
+    /// binding.  ("implicit" = without an explicit `impl Foo<T> {}` - see
+    /// <https://cxx.rs/extern-c++.html?highlight=explicit#explicit-shim-trait-impls>).
+    ///
+    /// The main consideration is avoiding introducing conflicting/overlapping impls:
+    ///
+    /// * The `cxx` crate already provides impls for cases where `T` is a primitive
+    ///   type like `u32`
+    /// * Some generics (e.g. Rust bindings for C++ templates like `CxxVector<T>`, `UniquePtr<T>`,
+    ///   etc.) require an `impl` of a `trait` provided by the `cxx` crate (such as
+    ///   [`cxx::vector::VectorElement`] or [`cxx::memory::UniquePtrTarget`]).  To avoid violating
+    ///   [Rust orphan rule](https://doc.rust-lang.org/reference/items/implementations.html#r-items.impl.trait.orphan-rule.intro)
+    ///   we restrict `T` to be a local type
+    ///   (TODO: or a fundamental type like `Box<LocalType>`).
+    /// * Other generics (e.g. C++ bindings for Rust generics like `Vec<T>` or `Box<T>`)
+    ///   don't necessarily need to follow the orphan rule, but we conservatively also
+    ///   only generate implicit impls if `T` is a local type.  TODO: revisit?
+    pub(crate) fn is_implicit_impl_ok(&self, types: &Types) -> bool {
+        match self {
+            ImplKey::RustBox(ident)
+            | ImplKey::RustVec(ident)
+            | ImplKey::UniquePtr(ident)
+            | ImplKey::SharedPtr(ident)
+            | ImplKey::WeakPtr(ident)
+            | ImplKey::CxxVector(ident) => types.is_local(ident.rust),
+        }
+    }
 }
 
 pub(crate) struct NamedImplKey<'a> {
