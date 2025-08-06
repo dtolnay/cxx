027855f7c5eab99a8df8c4aa651f2e4ceb65e72b
Mike Aizatsky <maizatskyi@cloudflare.com>
2025-08-06 10:24:21 -0700

    [workerd-cxx] README update
    updating readme, removing unmaintained docs and examples

--- book/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import pluginJs from '@eslint/js';
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { ignores: ['build/*'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  pluginJs.configs.recommended,
-];
