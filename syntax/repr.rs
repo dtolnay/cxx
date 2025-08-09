d7e4d48aa4ca141278da20a6a921770955977405
David Tolnay <dtolnay@gmail.com>
2025-08-09 09:06:21 -0700

    Report alignment value error on the integer literal

--- syntax/repr.rs
+++ syntax/repr.rs
@@ -22,16 +22,17 @@ impl Parse for Repr {
         } else if ident == "align" {
             let content;
             parenthesized!(content in input);
-            let align: u32 = content.parse::<LitInt>()?.base10_parse()?;
+            let align_lit: LitInt = content.parse()?;
+            let align: u32 = align_lit.base10_parse()?;
             if !align.is_power_of_two() {
                 return Err(Error::new_spanned(
-                    begin.token_stream(),
+                    align_lit,
                     "invalid repr(align) attribute: not a power of two",
                 ));
             }
             if align > 2u32.pow(29) {
                 return Err(Error::new_spanned(
-                    begin.token_stream(),
+                    align_lit,
                     "invalid repr(align) attribute: larger than 2^29",
                 ));
             }
