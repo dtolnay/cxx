41ec56f5023df69ba7e7e66fd634f61318e8bbfb
David Tolnay <dtolnay@gmail.com>
2025-08-09 21:12:54 -0700

    Release 1.0.168

--- src/lib.rs
+++ src/lib.rs
@@ -363,7 +363,7 @@
 //! </table>
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/cxx/1.0.167")]
+#![doc(html_root_url = "https://docs.rs/cxx/1.0.168")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(
     improper_ctypes,
