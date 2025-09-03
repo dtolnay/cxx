80cd48f55671e2b6efbad5cc619a6c5b18e19cbd
David Tolnay <dtolnay@gmail.com>
2025-09-03 16:08:10 -0700

    Ignore wrong_self_convention clippy lint

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -18,7 +18,8 @@
     clippy::too_many_arguments,
     clippy::too_many_lines,
     clippy::toplevel_ref_arg,
-    clippy::uninlined_format_args
+    clippy::uninlined_format_args,
+    clippy::wrong_self_convention
 )]
 #![allow(unknown_lints, mismatched_lifetime_syntaxes)]
 
