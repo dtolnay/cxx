be93ab23c882493172440b2bb86fcd51f8c69fdc
David Tolnay <dtolnay@gmail.com>
2025-09-06 09:55:29 -0700

    Add an UnpinReason for mutable slices

--- syntax/unpin.rs
+++ syntax/unpin.rs
@@ -1,13 +1,14 @@
 use crate::syntax::cfg::ComputedCfg;
 use crate::syntax::map::{OrderedMap, UnorderedMap};
 use crate::syntax::set::UnorderedSet;
-use crate::syntax::{Api, Enum, Receiver, Ref, Struct, Type, TypeAlias};
+use crate::syntax::{Api, Enum, NamedType, Receiver, Ref, SliceRef, Struct, Type, TypeAlias};
 use proc_macro2::Ident;
 
 #[allow(dead_code)] // only used by cxxbridge-macro, not cxx-build
 pub(crate) enum UnpinReason<'a> {
     Receiver(&'a Receiver),
     Ref(&'a Ref),
+    Slice(&'a SliceRef),
 }
 
 pub(crate) fn required_unpin_reasons<'a>(
@@ -20,16 +21,27 @@ pub(crate) fn required_unpin_reasons<'a>(
 ) -> UnorderedMap<&'a Ident, UnpinReason<'a>> {
     let mut reasons = UnorderedMap::new();
 
+    let is_extern_type_alias = |ty: &NamedType| -> bool {
+        cxx.contains(&ty.rust)
+            && !structs.contains_key(&ty.rust)
+            && !enums.contains_key(&ty.rust)
+            && aliases.contains_key(&ty.rust)
+    };
+
+    for (ty, _cfgs) in all {
+        if let Type::SliceRef(slice) = ty {
+            if let Type::Ident(inner) = &slice.inner {
+                if slice.mutable && is_extern_type_alias(inner) {
+                    reasons.insert(&inner.rust, UnpinReason::Slice(slice));
+                }
+            }
+        }
+    }
+
     for api in apis {
         if let Api::CxxFunction(efn) | Api::RustFunction(efn) = api {
             if let Some(receiver) = efn.receiver() {
-                if receiver.mutable
-                    && !receiver.pinned
-                    && cxx.contains(&receiver.ty.rust)
-                    && !structs.contains_key(&receiver.ty.rust)
-                    && !enums.contains_key(&receiver.ty.rust)
-                    && aliases.contains_key(&receiver.ty.rust)
-                {
+                if receiver.mutable && !receiver.pinned && is_extern_type_alias(&receiver.ty) {
                     reasons.insert(&receiver.ty.rust, UnpinReason::Receiver(receiver));
                 }
             }
@@ -39,13 +51,7 @@ pub(crate) fn required_unpin_reasons<'a>(
     for (ty, _cfg) in all {
         if let Type::Ref(ty) = ty {
             if let Type::Ident(inner) = &ty.inner {
-                if ty.mutable
-                    && !ty.pinned
-                    && cxx.contains(&inner.rust)
-                    && !structs.contains_key(&inner.rust)
-                    && !enums.contains_key(&inner.rust)
-                    && aliases.contains_key(&inner.rust)
-                {
+                if ty.mutable && !ty.pinned && is_extern_type_alias(inner) {
                     reasons.insert(&inner.rust, UnpinReason::Ref(ty));
                 }
             }
