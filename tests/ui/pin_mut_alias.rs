ef9246d38db11d5216e4208ac8f3a56528b91fd4
David Tolnay <dtolnay@gmail.com>
2025-09-05 20:20:43 -0700

    Split !Unpin test by usage kind

--- tests/ui/pin_mut_alias.rs
+++ tests/ui/pin_mut_alias.rs
@@ -1,20 +1,60 @@
-use cxx::ExternType;
-use std::marker::PhantomPinned;
+mod arg {
+    use cxx::ExternType;
+    use std::marker::PhantomPinned;
 
-struct Opaque(PhantomPinned);
+    struct Arg(PhantomPinned);
 
-unsafe impl ExternType for Opaque {
-    type Id = cxx::type_id!("Opaque");
-    type Kind = cxx::kind::Opaque;
+    unsafe impl ExternType for Arg {
+        type Id = cxx::type_id!("Arg");
+        type Kind = cxx::kind::Opaque;
+    }
+
+    #[cxx::bridge]
+    mod ffi {
+        unsafe extern "C++" {
+            type Arg = crate::arg::Arg;
+            fn f(arg: &mut Arg);
+        }
+    }
 }
 
-#[cxx::bridge]
-mod ffi {
-    unsafe extern "C++" {
-        type Opaque = crate::Opaque;
-        fn f(arg: &mut Opaque);
-        fn g(&mut self);
-        fn h(self: &mut Opaque);
+mod receiver {
+    use cxx::ExternType;
+    use std::marker::PhantomPinned;
+
+    struct Receiver(PhantomPinned);
+
+    unsafe impl ExternType for Receiver {
+        type Id = cxx::type_id!("Receiver");
+        type Kind = cxx::kind::Opaque;
+    }
+
+    #[cxx::bridge]
+    mod ffi {
+        unsafe extern "C++" {
+            type Receiver = crate::receiver::Receiver;
+            fn g(&mut self);
+        }
+    }
+}
+
+mod receiver2 {
+    use cxx::ExternType;
+    use std::marker::PhantomPinned;
+
+    struct Receiver2(PhantomPinned);
+
+    unsafe impl ExternType for Receiver2 {
+        type Id = cxx::type_id!("Receiver2");
+        type Kind = cxx::kind::Opaque;
+    }
+
+    #[cxx::bridge]
+    mod ffi {
+        unsafe extern "C++" {
+            type Receiver2 = crate::receiver2::Receiver2;
+            fn h(self: &mut Receiver2);
+        }
     }
 }
 
