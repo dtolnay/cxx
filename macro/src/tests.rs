3f0919fb6e42f98990cd9e7936f6aa35e27ba185
Lukasz Anforowicz <lukasza@chromium.org>
2025-11-17 22:12:38 +0000

    Add test coverage for `UniquePtr<StructWithLifetime<>>`.

--- macro/src/tests.rs
+++ macro/src/tests.rs
@@ -7,7 +7,14 @@ use syn::File;
 fn bridge(cxx_bridge: TokenStream) -> String {
     let module = syn::parse2::<Module>(cxx_bridge).unwrap();
     let tokens = expand::bridge(module).unwrap();
-    let file = syn::parse2::<File>(tokens).unwrap();
+    let file = match syn::parse2::<File>(tokens.clone()) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("The code below is syntactically invalid: {err}:");
+            eprintln!("{tokens}");
+            panic!("`expand::bridge` should generate syntactically valid code");
+        }
+    };
     let pretty = prettyplease::unparse(&file);
     eprintln!("{0:/<80}\n{pretty}{0:/<80}", "");
     pretty
@@ -99,4 +106,34 @@ fn test_mangling_covers_cpp_namespace_of_vec_elements() {
     // in thunk names used for two types with the same name, but in a different
     // namespace.
     assert!(rs.contains("export_name = \"cxxbridge1$rust_vec$test_namespace$Context$set_len\""));
+}
+
+#[test]
+fn test_struct_with_lifetime() {
+    let rs = bridge(quote! {
+        mod ffi {
+            struct StructWithLifetime<'a> {
+                s: &'a str,
+            }
+            extern "Rust" {
+                fn f(_: UniquePtr<StructWithLifetime<>>);
+            }
+        }
+    });
+
+    // This is mostly a regression test for problems that were accidentally introduced
+    // in https://github.com/dtolnay/cxx/pull/1658 which for the input above would
+    // generate syntatically invalid code:
+    //
+    //     impl<'a> ::cxx::memory::UniquePtrTarget for StructWithLifetime < > < 'a > {
+    //
+    // In presence of syntax error the test helper `bridge` called above will panic,
+    // but for completeness the assertion below verifies that correct code has been generated.
+    assert!(rs.contains("impl<'a> ::cxx::memory::UniquePtrTarget for StructWithLifetime<'a> {"));
+
+    // Assertions for to other places that refer to `StructWithLifetime`.
+    assert!(rs.contains("pub struct StructWithLifetime<'a> {"));
+    assert!(rs.contains("cast::<StructWithLifetime<'a>>()"));
+    assert!(rs.contains("fn __f(arg0: ::cxx::UniquePtr<StructWithLifetime>) {"));
+    assert!(rs.contains("impl<'a> self::Drop for super::StructWithLifetime<'a>"));
 }
